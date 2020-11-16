96b7be915d74ffae44302a7ead725100b697567a
eliadka <73880800+eliadka@users.noreply.github.com>
2020-11-16 16:47:48 +0300

    Update execise4_7.py

--- execise4_7.py
+++ execise4_7.py
@@ -1,13 +1,18 @@
-Реализовать два небольших скрипта:
-а) бесконечный итератор, генерирующий целые числа,
-начиная с указанного,
-б) бесконечный итератор, повторяющий элементы
-некоторого списка, определенного заранее.
-Подсказка: использовать функцию count() и cycle()
-модуля itertools.
-'''
-
 from itertools import count
-from itertools import cycle
+from math import factorial
+#################
+def fibo_gen(num):
+    i = 1
+    while i <= num:
+        i += 1
+        yield i
 
-
+q = 1
+my_fifteen = []
+for el in fibo_gen(5):
+    if q > 15:
+        break
+    else:
+        my_fifteen.append(el)
+        q +=1
+print(my_fifteen)
