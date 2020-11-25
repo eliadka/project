382c61852cd64d1e2fc0145e3a585ee9174cb85e
ElenaB <lena.korotkih@gmail.com>
2020-11-25 15:13:35 +0300

    Lesson7

---
+++ execise7_1.py
@@ -0,0 +1,32 @@
+class Matrix:
+    def __init__(self, list_1, list_2):
+        self.list_1 = list_1
+        self.list_2 = list_2
+
+    def __add__(self):
+        matr = [[0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0]]
+
+        for i in range(len(self.list_1)):
+
+            for j in range(len(self.list_2[i])):
+                matr[i][j] = self.list_1[i][j] + self.list_2[i][j]
+
+        return str('\n'.join(['\t'.join([str(j) for j in i]) for i in matr]))
+
+
+    def __str__(self):
+        return str('\n'.join(['\t'.join([str(j) for j in i]) for i in matr]))
+
+
+
+my_matrix = Matrix([[2, 18, 13],
+                    [4, 7, 3],
+                    [51, 40, 9]],
+                   [[5, 8, 52],
+                    [6, 7, 13],
+                    [4, 45, 34]])
+
+
+print(my_matrix.__add__())
