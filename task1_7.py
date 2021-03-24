1c123bfc852ad4c0ee18741f6746a9a717c2c5a3
ElenaB <lena.korotkih@gmail.com>
2021-03-24 13:20:12 +0300

    d

---
+++ task1_7.py
@@ -0,0 +1,46 @@
+import re
+
+
+class DequeClass:
+    def __init__(self):
+        self.elems = []
+
+    def is_empty(self):
+        return self.elems == []
+
+    def add_to_front(self, elem):
+        self.elems.append(elem)
+
+    def add_to_rear(self, elem):
+        self.elems.insert(0, elem)
+
+    def remove_from_front(self):
+        return self.elems.pop()
+
+    def remove_from_rear(self):
+        return self.elems.pop(0)
+
+    def size(self):
+        return len(self.elems)
+
+
+def pal_checker(string):
+    dc_obj = DequeClass()
+    new_string = ''.join(string.split())    # вариант разбирать строку и собирать ее
+    re_string = ''.join(re.split(r' ', string)) # вариант с регулярными выражениями
+
+    for el in re_string:
+        dc_obj.add_to_rear(el)
+
+    still_equal = True
+
+    while dc_obj.size() > 1 and still_equal:
+        first = dc_obj.remove_from_front()
+        last = dc_obj.remove_from_rear()
+        if first != last:
+            still_equal = False
+
+    return still_equal
+
+
+print(pal_checker("молоко делили ледоколом"))
